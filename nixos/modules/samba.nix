40b2ef5134f44caf92faafcd9e745179fa0548af
David <20436145+Master449@users.noreply.github.com>
2024-05-25 14:53:40 -0500

    Moving nix to its own repository

--- nixos/modules/samba.nix
+++
@@ -1,9 +0,0 @@
-{ lib, config, pkgs, ... }:
-
-{
-  fileSystems."/mnt/media" = {
-    device = "\\192.168.5.218";
-    fsType = "cfis";
-    options = [ "username=david" "password=Some12hold" "x-systemd.automound" "noauto" ];
-  };
-}
