290c989c4797ef37096f56b5010d83504169e3e8
David Flowers II <20436145+Master449@users.noreply.github.com>
2025-01-05 13:30:27 -0600

    I wish tmux just worked, that would be amazing

--- hooks/qemu.d/win11/prepare/begin/start.sh
+++ hooks/qemu.d/win11/prepare/begin/start.sh
@@ -1,4 +1,4 @@
-#!/run/current-system/sw/bin/bash
+#!/bin/bash
 
 #debugging
 set -x
@@ -12,18 +12,17 @@ exec 1>/home/david/Desktop/startlog.out 2>&1
 #systemctl set-property --runtime -- init.scope AllowedCPUs=8-15,24-31
 
 # Kill Display Manager
-/nix/store/gw98rbwk3f19pvv1610kyjn8h5v53834-system-path/bin/killall kwin_wayland
-systemctl stop display-manager
-systemctl stop sddm
-
+#/nix/store/gw98rbwk3f19pvv1610kyjn8h5v53834-system-path/bin/killall kwin_wayland
+systemctl stop gdm3
+systemctl stop gdm
 # Sleep to let those changes apple
 sleep 10
 
-echo 0 > /sys/class/vtconsole/vtcon0/bind
-echo 0 > /sys/class/vtconsole/vtcon1/bind
+#echo 0 > /sys/class/vtconsole/vtcon0/bind
+#echo 0 > /sys/class/vtconsole/vtcon1/bind
 
 # uncomment the next line if you're getting a black screen
-echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+#echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
 # Unload AMD and Intel Audio
 modprobe -r amdgpu
